63d2a4744c4d62590069ebd7048b96d4d762c677
Dmytry Ilin <ilin.dmitry279@gmail.com>
2015-08-28 08:57:46 +0300

    added alert.py
    sms sender for rb751

---
+++ alert.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+#coding=utf-8
+import telnetlib
+def mikroticconnect(num,mes):
+    #import pdb; pdb.set_trace()
+    host = '172.16.0.253'
+    tn = telnetlib.Telnet(host)
+    out = tn.read_until('Login: ',5)
+    tn.write("admin\r\n")
+    out = tn.read_until('Password: ',5)
+    tn.write("\r\n")
+    out = tn.read_until('[admin@LabZV] > ',10)
+    from os import system
+    from sys import platform
+    if platform == 'win32':
+        sleep = 'timeout /T 5 > NUL'
+    else:
+        sleep = 'sleep 5'
+    system(sleep)
+    gatemess = '/tool sms send port=usb1 channel=0 phone-number=' + num + ' message="' + mes + '" type=class-0\r\n'
+    tn.write(gatemess)
+    system(sleep)
+    out = tn.read_until('[admin@LabZV] >',20)
+    tn.close()
+    return ''
+
+
+if __name__ == '__main__':
+    import sys
+    num = str(sys.argv[1])
+    mes = str(sys.argv[2])
+    mikroticconnect(num,mes)
